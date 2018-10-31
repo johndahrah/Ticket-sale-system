1e222b6dd8e479d3b20fd688159c2c524241dfb3
johndahrah <dahrah@yandex.ru>
2018-10-31 22:16:15 +0300

    source code structuring using Flask's "blueprints" paradigm

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, request, jsonify
-from sqlalchemy import *
-from sqlalchemy.exc import DataError
-from sqlalchemy.engine import ResultProxy
 import re
-import datetime
-import json_text_constants as j_const
+
+from flask import Flask
+from sqlalchemy import *
+
 import generate_test_db_data
 
 app = Flask(__name__)
@@ -22,289 +20,23 @@ db = create_engine(
     )
 
 
-@app.route('/api/user/add', methods=['POST'])
-def user_add():
-    content = dict(request.get_json())
-    username, password, level = get_user_data(content)
-
-    db.execute(
-        "INSERT INTO Users "
-        "(login, password, accessLevel) "
-        "VALUES (\'%s\', \'%s\', %s);"
-        % (username, password, level)
-        )
-    return 'ok'
-
-
-@app.route('/api/user/modify', methods=['GET', 'POST'])
-def user_modify():
-    # todo check if the user is in the database
-    content = dict(request.get_json())
-    username, password, level = get_user_data(content)
-    data_to_change = content.get(j_const.change_data_type)
-
-    if data_to_change == j_const.level:
-        db.execute(
-            f'UPDATE users SET accesslevel = {level} '
-            f'WHERE login LIKE \'{username}\''
-            )
-        return 'ok'
-    if data_to_change == j_const.password:
-        db.execute(
-            f'UPDATE users SET password = {password} '
-            f'WHERE login LIKE \'{username}\''
-            )
-        return 'ok'
-    if data_to_change == j_const.username:
-        # todo username change
-        pass
-    return j_const.json_change_value_error % data_to_change
-
-
-@app.route('/api/ticket/view/all', methods=['GET'])
-def ticket_view_all():
-    result = db.execute(
-        'SELECT * FROM tickets'
-        )
-    return jsonify({'result': [dict(row) for row in result]})
-
-
-@app.route('/api/ticket/view', methods=['GET'])
-def ticket_view_specific():
-    if len(request.args) == 0:
-        return ticket_view_all()
-
-    sql_statement = 'SELECT * FROM tickets'
-    # the following line is for preventing IDE from "unexpected eol" warning
-    sql_statement += ' WHERE '
-    multiple_parameters = False
-    at_least_one_argument = False
-    for json_key in j_const.all_properties:
-        argument = request.args.get(json_key)
-        if argument is not None:
-            at_least_one_argument = True
-            equal_operator = '=' if argument.isdigit() else 'LIKE'
-            if multiple_parameters:
-                sql_statement += ' AND '
-            sql_statement += f'{json_key} {equal_operator} {argument}'
-            multiple_parameters = True
-    if at_least_one_argument:
-        result = db.execute(sql_statement)
-        return jsonify({'result': [dict(row) for row in result]})
-    else:
-        return 'no one correct json key found'
-
-
-@app.route('/api/ticket/view/<ticket_id>')
-def ticket_view_by_id(ticket_id):
-    sql_statement = f'SELECT * FROM tickets ' \
-                    f'WHERE id = {ticket_id}'
-    result = db.execute(sql_statement);
-    return jsonify({'result': [dict(row) for row in result]})
-
-
-@app.route('/api/ticket/add', methods=['GET', 'POST'])
-def ticket_add():
-    i = dict(request.get_json())
-    try:
-        sql_statement = f'INSERT INTO tickets ' \
-                        f'(OpenedForSelling, EventDate, EventTime, ' \
-                        f'EventPlace, EventOrganizerName, SellPrice, ' \
-                        f'Comment, OrganizerID, SerialNumber, isSold) ' \
-                        f'VALUES (' \
-                        f'{i[j_const.opened]}, ' \
-                        f'\'{i[j_const.date]}\', ' \
-                        f'\'{i[j_const.time]}\', ' \
-                        f'\'{i[j_const.place]}\', ' \
-                        f'\'{i[j_const.organizer]}\', ' \
-                        f'{i[j_const.price]}, ' \
-                        f'\'{i[j_const.comment]}\', ' \
-                        f'\'{i[j_const.organizerid]}\', ' \
-                        f'\'{i[j_const.serial]}\', {False}' \
-                        f');'
-    except KeyError as e:
-        return f'invalid json key has been received, check: {e}'
-    try:
-        db.execute(sql_statement)
-    except DataError as e:
-        return f'invalid type! Details: {e}'
-    return 'successful'
-
-
-def at_least_one_ticket_is_already_sold(tickets_id):
-    # todo:2 implement
-    return False
-
-
-def at_least_one_ticket_is_closed_for_sale(tickets_id):
-    try:
-        for i in tickets_id:
-            sql_statement = f'SELECT * FROM tickets ' \
-                            f'WHERE id = {i} AND openedforselling = true'
-            result = receive_sql_query_result(sql_statement)
-            if result is None:
-                return True
-        return False
-    except TypeError:  # 'int' object is not iterable
-        sql_statement = f'SELECT * FROM tickets ' \
-                        f'WHERE id = {tickets_id} AND openedforselling = true'
-        result = receive_sql_query_result(sql_statement)
-        return result is None
-
-
-@app.route('/api/ticket/sell', methods=['POST'])
-def ticket_sell():
-    now = datetime.datetime.now()
-    date = str(now.date())
-    json = dict(request.json)
-    coupon_data = json.get(j_const.coupon)
-
-    # separate the cases of (only one) and (many) tickets being sold:
-    #  in case of one ticket, we operate with it as an "int"
-    #  in case of many tickets, we operate with them as a "tuple"
-    tickets_ids = json.get(j_const.sell_tickets_id)
-    if type(tickets_ids) is int:
-        tickets_id = tickets_ids
-    else:
-        tickets_id = tuple(json.get(j_const.sell_tickets_id))
-
-    if at_least_one_ticket_is_already_sold(tickets_id):
-        return 'One or more chosen tickets are already sold'
-
-    if at_least_one_ticket_is_closed_for_sale(tickets_id):
-        return 'One or more chosen tickets are closed for sale'
-
-    # insert the possible coupon data into the database
-    if coupon_data is not None:
-        sql_statement = f'INSERT INTO coupons ' \
-                        f'(dateUsed, couponData) ' \
-                        f'VALUES (\'{date}\', \'{coupon_data}\')'
-        db.execute(sql_statement)
-
-    # mark necessary tickets as "sold"
-    equal_operator = '=' if type(tickets_ids) is int else 'IN'
-    sql_statement = f'UPDATE tickets ' \
-                    f'SET issold = {True} ' \
-                    f'WHERE id {equal_operator} {tickets_id}'
-    db.execute(sql_statement)
-
-    # receive the total price
-    sql_statement = f'SELECT sum(sellprice) FROM tickets ' \
-                    f'WHERE id {equal_operator} {tickets_id}'
-    total_price = receive_sql_query_result(sql_statement)
-
-    worker_id = json.get(j_const.userID)
-
-    # receive coupon data
-    sql_statement = f'SELECT id FROM coupons ' \
-                    f'WHERE coupondata LIKE \'{coupon_data}\''
-    coupon_id = receive_sql_query_result(sql_statement)
-
-    # insert all data in "checks" table and receive the id of a new record
-    sql_statement = f'INSERT INTO checks ' \
-                    f'(TicketsAmount, TotalPrice, ' \
-                    f'CouponUsed, CouponID, WorkerID) ' \
-                    f'VALUES (' \
-                    f'{1 if type (tickets_ids) is int else len(tickets_id)},' \
-                    f'{total_price}, {coupon_data is not None}, ' \
-                    f'{"null" if coupon_id is None else coupon_id}, ' \
-                    f'{worker_id}) ' \
-                    f'RETURNING id'
-    check_id = receive_sql_query_result(sql_statement)
-
-    # create a one-to-many relationship check<->ticket
-    try:
-        for ticket_id in tickets_id:
-            sql_statement = f'INSERT INTO check_ticketsid VALUES ' \
-                            f'({check_id}, {ticket_id})'
-            db.execute(sql_statement)
-    except TypeError:  # 'int' object is not iterable
-        sql_statement = f'INSERT INTO check_ticketsid VALUES ' \
-                        f'({check_id}, {tickets_id})'
-        db.execute(sql_statement)
-    return 'ok'
-
-
-def receive_sql_query_result(sql_statement):
-    """
-    works only for receiving a single variable.
-    actually needs to be rewritten
-    """
-    sql_sum_result: ResultProxy = db.execute(sql_statement)
-    result = []
-    if sql_sum_result.rowcount != 1:
-        print('--- CHECK receive_sql_query_result ---')
-    for row in sql_sum_result:
-        result.append(row)
-
-    try:
-        data = result[0]._row[0]
-    except IndexError:
-        return None
-    return data
-
-
-@app.route('/api/ticket/modify/<ticket_id>', methods=['GET', 'POST'])
-def ticket_modify(ticket_id):
-    if not ticket_id_exists(ticket_id):
-        return 'no ticket with such ID'
-
-    sql_statement = f'UPDATE tickets SET '
-
-    json_dict = dict(request.get_json())
-    if json_dict.get('id') is not None:
-        return 'you can not modify ticket\'s ID'
-
-    more_than_one_argument = False
-    for i in j_const.all_properties:
-        argument = json_dict.get(i)
-        if argument is not None:
-            if more_than_one_argument:
-                sql_statement += ' , '
-            sql_statement += f'{i} = {argument}'
-            more_than_one_argument = True
-    sql_statement += f'WHERE id = {ticket_id}'
-    db.execute(sql_statement)
-    return 'ok'
-
-
-def ticket_id_exists(ticket_id):
-    sql_check_id_statement = f'SELECT * FROM tickets ' \
-                             f'WHERE id = {ticket_id}'
-    result_of_check: ResultProxy = db.execute(sql_check_id_statement)
-    rows_number = result_of_check.rowcount
-    return rows_number != 0
+from handlers.TicketsHandler import tickets_handler
+from handlers.UsersHandler import users_handler
+app.register_blueprint(tickets_handler)
+app.register_blueprint(users_handler)
 
 
 @app.route('/api/system/db/init_tables')
 def initialize_all_tables():
-    content = read_file('create_tables.sql')
+    content = read_file('sql/create_tables.sql')
     return execute_sql_commands(content)
 
 
 @app.route('/api/system/db/drop_tables')
 def drop_all_tables():
     # todo: a good permission check is vital for this action
-    content = read_file('drop_all_tables.sql')
+    content = read_file('sql/drop_all_tables.sql')
     return execute_sql_commands(content)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/api/json_example', methods=['POST'])
-def example():
-    content = request.json
-    return jsonify(content)
-
-
-def get_user_data(content):
-    username = content.get(j_const.username)
-    password = content.get(j_const.password)
-    level = content.get(j_const.level)
-    return username, password, level
 
 
 def read_file(filename):
---
+++ handlers/UsersHandler.py
@@ -0,0 +1,54 @@
+from flask import request, Blueprint
+
+import json_text_constants as j_const
+from app import db
+
+users_handler = Blueprint(
+    'users_handler', __name__, url_prefix='/api/user'
+    )
+
+
+@users_handler.route('/add', methods=['POST'])
+def user_add():
+    content = dict(request.get_json())
+    username, password, level = get_user_data(content)
+
+    db.execute(
+        "INSERT INTO Users "
+        "(login, password, accessLevel) "
+        "VALUES (\'%s\', \'%s\', %s);"
+        % (username, password, level)
+        )
+    return 'ok'
+
+
+@users_handler.route('/modify', methods=['GET', 'POST'])
+def user_modify():
+    # todo check if the user is in the database
+    content = dict(request.get_json())
+    username, password, level = get_user_data(content)
+    data_to_change = content.get(j_const.change_data_type)
+
+    if data_to_change == j_const.level:
+        db.execute(
+            f'UPDATE users SET accesslevel = {level} '
+            f'WHERE login LIKE \'{username}\''
+            )
+        return 'ok'
+    if data_to_change == j_const.password:
+        db.execute(
+            f'UPDATE users SET password = {password} '
+            f'WHERE login LIKE \'{username}\''
+            )
+        return 'ok'
+    if data_to_change == j_const.username:
+        # todo username change
+        pass
+    return j_const.json_change_value_error % data_to_change
+
+
+def get_user_data(content):
+    username = content.get(j_const.username)
+    password = content.get(j_const.password)
+    level = content.get(j_const.level)
+    return username, password, level
