94196abea81fb38aa1ff6594cfff79baf95c9999
johndahrah <dahrah@yandex.ru>
2018-11-06 19:28:30 +0300

    ticket search UI

---
+++ static/js/tickets_list.js
@@ -0,0 +1,8 @@
+function disableEmptyInputs(form) {
+  var controls = form.elements;
+  for (var i=0, iLen=controls.length; i<iLen; i++) {
+    controls[i].disabled = controls[i].value == '';
+  }
+}
+
+console.log("TEST");
