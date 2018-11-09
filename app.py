6df93cd214889198565fb1538bccdec44d99eef1
johndahrah <dahrah@yandex.ru>
2018-11-09 20:01:31 +0300

    coupons handler added

--- app.py
+++ app.py
@@ -23,9 +23,11 @@ db = create_engine(
 from handlers.TicketsHandler import tickets_handler
 from handlers.UsersHandler import users_handler
 from handlers.OrganizersHandler import organizers_handler
+from handlers.CouponsHandler import coupons_handler
 app.register_blueprint(tickets_handler)
 app.register_blueprint(users_handler)
 app.register_blueprint(organizers_handler)
+app.register_blueprint(coupons_handler)
 
 
 @app.route('/')
---
+++ handlers/CouponsHandler.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, abort
+
+from app import db
+
+coupons_handler = Blueprint (
+    'coupons_handler', __name__, url_prefix='/api/coupon'
+    )
+
+
+@coupons_handler.route('/view/all')
+def coupons_view_all():
+    result_sys = db.execute(
+        'SELECT * FROM coupons'
+        )
+    result = [dict(row) for row in result_sys]
+    return render_template('coupons_list.html', attributes=result)
+
+
+@coupons_handler.route('/add')
+def coupons_add():
+    """
+    coupons table is automatically updated on tickets_sell() method.
+    todo: replace with @app.errorhandler()
+    """
+    return abort(405)
+
+
