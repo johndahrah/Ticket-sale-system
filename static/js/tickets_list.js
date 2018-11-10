f4b6b8bd6d796f8e1a72ca76e9b4f62e981861d2
johndahrah <dahrah@yandex.ru>
2018-11-10 21:36:08 +0300

    select tickets js implementation

--- static/js/tickets_list.js
+++ static/js/tickets_list.js
@@ -1,3 +1,5 @@
+let selected_tickets = [];
+
 function disableEmptyInputs(form) {
     const controls = form.elements;
 
@@ -11,4 +13,27 @@ function disableEmptyInputs(form) {
 
 function sellTickets() {
     
+}
+
+function selectTickets(table) {
+    const id = table.id;
+
+    if (selected_tickets.includes(id)) {
+        table.style.borderWidth="1px";
+        table.style.marginBottom="20px";
+        selected_tickets = arrayRemove(selected_tickets, id)
+    } else {
+        table.style.borderWidth="3px";
+        table.style.marginBottom="16px";
+        selected_tickets.push(id);
+    }
+
+    selected_tickets.sort(function(a, b){return a - b});
+    console.log(selected_tickets)
+}
+
+function arrayRemove(arr, value) {
+   return arr.filter(function(elem){
+       return elem !== value;
+   });
 }
