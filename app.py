112f534a23164f721449bceb2d78cf5c312059b3
johndahrah <dahrah@yandex.ru>
2018-11-11 16:58:54 +0300

    heroku postgres support (specific port)

--- app.py
+++ app.py
@@ -76,4 +76,5 @@ initialize_all_tables()
 generate_test_db_data.generate(db, clear_existing=true)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
